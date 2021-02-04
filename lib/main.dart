eef9e47cab616c7a226ff5cf1df338ad7107eeda
nld-8832 <linhnhat.dang9976@gmail.com>
2021-02-04 10:58:52 +0700

    Simplified UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,159 +1,180 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-
 import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Pomodoro(),
     );
   }
 }
 
 class Pomodoro extends StatefulWidget {
+  //Pomodoro({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  //final String title;
+
   @override
   _PomodoroState createState() => _PomodoroState();
 }
 
-class _PomodoroState extends State<Pomodoro> {
-  static int timeSession = 25;
-  int timeSessionInSec = timeSession * 60;
-  double percent = 0;
+class _PomodoroState extends State<Pomodoro> with TickerProviderStateMixin {
+  int _timeLeft = 1;
+  int _timeLeftInSec = 1 * 3;
+  bool _isRunning = false;
+  bool _isAtStart = true;
 
-  Timer timer;
+  int _breaktimeLeft = 2;
+  int _breaktimeLeftInSec = 2*1;
+  String _sessionStatus = "Work!";
+  int sessionCounter = 0;
 
-  _StartTimer() {
-    timeSession = 25;
-    int time = timeSession * 60;
-    double timePercent = time/100;
+  /*
+  void sessionCounterProcess() {
+    if (sessionCounter = 4) {
 
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (time > 0) {
-          time--;
-          if (time % 60 == 0) {
-            timeSession--;
-          }
-          if (time % timePercent == 0) { // Progress bar percent calculation
-            if (percent < 1) {
-              percent += 0.01;
-            } else {
-              percent = 1;
-            }
-          }
-        } else {
-          percent = 0;
-          timeSession = 25;
-          timer.cancel();
-        }
-      });
-    });
+    }
+  }
+  */
+
+  // Animation processing
+  AnimationController _controller;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: _timeLeftInSec),
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reset();
+        sessionCounter++;
+      }
+    });
+    super.initState();
+  }
+
+  // UI
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xff1542bf), Color(0xff51a8ff)],
-              begin: FractionalOffset(0.5, 1),
-            ),
-          ),
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Text("Pomodoro?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                  ),
-                ),
-              ),
-              Expanded(child: CircularPercentIndicator(
-                  percent: percent,
-                  animation: true,
-                  animateFromLastPercent: true,
-                  radius: 250.0,
-                  lineWidth: 10.0,
-                  progressColor: Colors.white,
-                  center: Text("$timeSession",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 80.0,
-                    ),
-                  ),
-              )),
-              SizedBox(height: 30.0),
-              Expanded(child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top: 30.0,
-                    left: 30.0,
-                    right: 30.0,
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Expanded(child: Row(
-                        children: <Widget>[
-                          Expanded(child: Column(
-                            children: <Widget>[
-                              Text("Pomodoro Timer",
-                                style: TextStyle(fontSize: 25.0)
-                              ),
-                              SizedBox(height: 10.0),
-                              Text("25",
-                                style: TextStyle(fontSize: 70.0),
-                              ),
-                            ],
-                          )),
-                        ],
-                      )),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 28.0),
-                        child: RaisedButton(
-                          onPressed: _StartTimer,
-                          color: Colors.blue,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100.0)),
-                          child: Padding(
-                            padding: EdgeInsets.all(20.0),
-                            child: Text("Start",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22.0,
-                              ),
-                            )
-                          ),
-                        )
-                      )
-                    ],
-                  ),
-                ),
-              )),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Current Session",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontFamily: "Roboto",
           ),
         ),
       ),
+      body: Center(
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Countdown(
+              animation: StepTween(
+                begin: _timeLeftInSec, // THIS IS AN USER ENTERED NUMBER
+                end: 0,
+              ).animate(_controller),
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: _isAtStart ? null : () {
+                _controller.reset();
+                _isAtStart = true;
+                setState(() => _isRunning = false);
+              },
+            ),
+            IconButton(
+              icon: (_isRunning) ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+              onPressed: () {
+                _isAtStart = false;
+                if (_isRunning) _controller.stop();
+                else _controller.forward();
+                setState(() => _isRunning = !_isRunning);
+              },
+            ),
+            Text("$_sessionStatus",
+              style: TextStyle(
+                color: Color(0xff929da5),
+                fontSize: 20,
+                letterSpacing: 1,
+              ),
+            ),
+          ],
+        ),
+      ),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.*/
     );
   }
 }
 
+// Custom class for animation
+class Countdown extends AnimatedWidget {
+  Countdown({Key key, this.animation}) : super(key: key, listenable: animation);
+  Animation<int> animation;
+
+  @override
+  build(BuildContext context) {
+    Duration duration = Duration(seconds: animation.value);
+
+    String timeLeft =
+        '${duration.inMinutes.remainder(60).toString()}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+
+    //print('animation.value  ${animation.value} ');
+
+    return Text(
+      "$timeLeft",
+      style: TextStyle(
+        fontSize: 70,
+        color: Color(0xff929da5),
+        fontFamily: "Roboto",
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
 
